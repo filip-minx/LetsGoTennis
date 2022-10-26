574c075e5b447e0d229dbd5972f07eb847eb39b6
Filip Minx <filip.minx2@gmail.com>
2022-10-26 15:40:17 +0200

    Add magic

--- hackathon/api/api.go
+++ hackathon/api/api.go
@@ -13,17 +13,21 @@ func GetCourts(c *gin.Context) {
 func PostReservation(c *gin.Context) {
 	var newReservation Reservation
 
-	// Call BindJSON to bid the received JSON to
-	// newAlbum.
 	if err := c.BindJSON(&newReservation); err != nil {
 		return
 	}
 
-	var durationInMs = newReservation.EndTime - newReservation.StartTime
-	var durationInMins = float64(durationInMs) / float64((60 * 60))
 	court := FirstOrDefault(CourtsList, func(p *Court) bool {
 		return p.Name == newReservation.CourtName
 	})
+
+	if !IsCourtAvailable(court, newReservation.StartTime, newReservation.EndTime) {
+		c.IndentedJSON(http.StatusTooEarly, newReservation)
+	}
+
+	var durationInMs = newReservation.EndTime - newReservation.StartTime
+	var durationInMins = float64(durationInMs) / float64((60 * 60))
+
 	var price = court.Price * float64(durationInMins)
 
 	if newReservation.PlayerCount > 3 {
@@ -35,7 +39,18 @@ func PostReservation(c *gin.Context) {
 	// Add the new album to the slice.
 	Reservations = append(Reservations, newReservation)
 	c.IndentedJSON(http.StatusCreated, newReservation)
+}
 
+func IsCourtAvailable(court *Court, startTime int64, endTime int64) bool {
+	var reservations = Where(Reservations, func(r *Reservation) bool {
+		return r.CourtName == court.Name
+	})
+
+	var res = FirstOrDefault(reservations, func(r *Reservation) bool {
+		return endTime > r.StartTime && startTime < r.EndTime
+	})
+
+	return res == nil
 }
 
 func GetReservations(c *gin.Context) {
@@ -51,4 +66,17 @@ func FirstOrDefault[T any](slice []T, filter func(*T) bool) (element *T) {
 	}
 
 	return nil
+}
+
+func Where[T any](slice []T, filter func(*T) bool) []T {
+
+	var ret []T = make([]T, 0)
+
+	for i := 0; i < len(slice); i++ {
+		if filter(&slice[i]) {
+			ret = append(ret, slice[i])
+		}
+	}
+
+	return ret
 }
